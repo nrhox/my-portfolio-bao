32f074153133238c686d0d6b1de51eb37831af1b
Jalu <jalunugroho118@gmail.com>
2025-10-28 18:51:56 +0700

    feat(views/PortofolioView): create my portofolio page but there is no content now

---
+++ src/views/PortofolioView.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import BaseLayout from '@/components/BaseLayout.vue'
+import { Icon } from '@iconify/vue'
+</script>
+
+<template>
+  <BaseLayout>
+    <template #page-name> Portofolio </template>
+    <div class="mt-4 flex h-96 items-center justify-center">
+      <div class="flex flex-col items-center">
+        <h3 class="mb-4 text-3xl">belum tersedia...</h3>
+        <Icon icon="fluent:window-dev-tools-16-regular" class="size-20 text-red-600" />
+      </div>
+    </div>
+  </BaseLayout>
+</template>
