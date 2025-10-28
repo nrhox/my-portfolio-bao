6dd243b7a2e6c25c22aa39a178aae64a314a8ff9
Jalu <jalunugroho118@gmail.com>
2025-10-28 18:47:40 +0700

    feat(components/ButtonBack): create component button back to previous page

---
+++ src/components/ButtonBack.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts">
+import { Icon } from '@iconify/vue'
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+</script>
+
+<template>
+  <button @click="router.back()">
+    <Icon icon="heroicons:arrow-left" class="size-6" />
+  </button>
+</template>
